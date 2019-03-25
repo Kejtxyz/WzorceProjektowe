3d0eb52376f641ac5ba9f5d7a2549b931099c945
kasia <kgbsz@wp.pl>
2019-03-25 18:45:31 +0100

    Wzorce projektowe - WZORCE KONSTRUKCYJNE

--- src/main/java/notatkaMain.java
+++ src/main/java/notatkaMain.java
@@ -1,3 +1,23 @@
 public class notatkaMain {
     // WZORCE PROJEKTOWE //
+
+    /**
+     * Disagne Patters
+     *
+     * -strukturalne (ulozenie komponentow i jak maja sie komunikowac - np. fasada
+     * -operacyjne,behawioralne, kto gdzie powinin miec jaka odpoweidzialnosc -  np. obserwator
+     * -szablonowa , klasa abstrakcyjna z funkcjonalnosciami i ona jest szablonem
+     * - wzorrzec to NIE jest konkretna implementacja, jest niezalezna od jezyka
+     *  - jest rozwiazaniem mentalnym ,logicznym problemu
+     *
+     * WZORCE KONSTRUKCYJNE - NAJPOPULARNIEJSZE:
+     * -FABRYKA ABSTRAKCYJNA
+     * -SINGLETON
+     * BUILDER
+     *
+     *ksiaka : wzroce projektowe
+     * - jesli natrafimy na problem,ktory gdzie moglby byc roziwazany, to wtedy szukamy w tej ksiazce, bo moze znajdziemy w tej ksiazce.
+     *
+     *
+     * */
 }
